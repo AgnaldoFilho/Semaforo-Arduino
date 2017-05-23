b4769ae250d882039d6981141ec00e6c9efec834
AgnaldoFilho <agnaldo.filho@live.com>
2017-05-22 22:54:30 -0300

    alterações sensor

--- asd.ino
+++ asd.ino
@@ -1,5 +1,9 @@
 #include <Thread.h>
 #include <ThreadController.h>
+#include <Ultrasonic.h>
+
+#define pino_trigger 11
+#define pino_echo 12
 
 int ledVerde1 =  10; //atribui o pino 10 a variável ledPin 
 int ledVerde2 =  7; //atribui o pino 13 a variável ledPin 
@@ -8,8 +12,13 @@ int ledAm2 =  6; //atribui o pino 13 a variável ledPin
 int ledVerm1 =  8; //atribui o pino 13 a variável ledPin 
 int ledVerm2 =  5; //atribui o pino 13 a variável ledPin 
 int dado; //variável que receberá os dados da porta serial
+int numCarros = 0;
+int altura = 9;
+int aux = 0;
 Thread threadControllerSemaforo = Thread();
 ThreadController controll = ThreadController();
+
+Ultrasonic ultrasonic(pino_trigger, pino_echo);
 
   void SemaforoController(){
    if(Serial.available() > 0){ //verifica se existe comunicação com a porta serial
@@ -66,4 +75,17 @@ void setup(){
  
 void loop(){
  controll.run();
+ float cmMsec, inMsec;
+  long microsec = ultrasonic.timing();
+  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  if(cmMsec < altura -1){
+    aux = cmMsec;
+    if(cmMsec <  altura + 1 && cmMsec > altura - 1 && cmMsec >  aux + 0,3){
+      numCarros ++;
+      Serial.println(cmMsec);
+      aux = altura;
+    }
+  }
+  
+  delay(50);
 }
